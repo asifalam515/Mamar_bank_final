3718680a81aefce5eefe4f9998cac02aa573633b
asifalam515 <asibulalam515@gmail.com>
2024-02-17 12:19:27 +0600

    email send feature done

--- transactions/views.py
+++ transactions/views.py
@@ -13,9 +13,24 @@ from django.db.models import Sum
 from django.shortcuts import get_object_or_404,redirect
 from django.views import View
 from django.urls import reverse_lazy
+from django.core.mail import EmailMessage,EmailMultiAlternatives
+from django.template.loader import render_to_string
 
 # Create your views here.
 #ei view ke deposit kore amara withdraw,deposit,loan_request korbo
+
+def send_transaction_email(user,amount,subject,template):
+       
+        message = render_to_string(template,{
+            'user':user,
+            'amount':amount
+        })
+        
+        send_email =EmailMultiAlternatives(subject,'',to =[user.email])
+        send_email.attach_alternative(message,"text/html")
+        send_email.send()
+    
+
 
 class TransactionCreateMixin(LoginRequiredMixin, CreateView):
     template_name = 'transactions/transaction_form.html'
@@ -60,7 +75,7 @@ class DepositMoneyView(TransactionCreateMixin):
             self.request,
             f'{"{:,.2f}".format(float(amount))}$ was deposited to your account successfully'
         )
-
+        send_transaction_email(self.request.user,amount,"Deposit Message",'transactions/deposit_email.html')
         return super().form_valid(form)
     
 class WithdrawMoneyView(TransactionCreateMixin):
@@ -83,7 +98,7 @@ class WithdrawMoneyView(TransactionCreateMixin):
             self.request,
             f'Successfully withdrawn {"{:,.2f}".format(float(amount))}$ from your account'
         )
-
+        send_transaction_email(self.request.user,amount,"Withdraw Message",'transactions/withdrawal_email.html')
         return super().form_valid(form)
 
 class LoanRequestView(TransactionCreateMixin):
@@ -104,6 +119,7 @@ class LoanRequestView(TransactionCreateMixin):
             self.request,
             f'Loan request for {"{:,.2f}".format(float(amount))}$ submitted successfully'
         )
+        send_transaction_email(self.request.user,amount,"Loan Request Message",'transactions/loan_email.html')
 
         return super().form_valid(form)
     
